073cd4d89403a85d655efb07abb84945f1d4c756
syriaHamster <the.syria.hamster@gmail.com>
2022-11-16 17:53:51 +0900

    추가: 스벨트 이해용 기초게임 설정

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
   import svelteLogo from "./assets/svelte.svg";
   import Counter from "./lib/Counter.svelte";
+  import Game from "./lib/Game.svelte";
 </script>
 
 <main>
@@ -16,6 +17,7 @@
 
   <div class="card">
     <Counter />
+    <Game />
   </div>
 
   <p>
---
+++ src/lib/Game.svelte
@@ -0,0 +1,46 @@
+<script>
+  let current = 1900;
+  let mode = "input"; //input, result
+  let showAnswer = 1945;
+
+  const onEnterHandler = (nextmode) => {
+    mode = nextmode;
+  };
+</script>
+
+<div>한국이 일본으로부터 독립한 년도는?</div>
+<div>
+  아마도 {current} 년?
+</div>
+{#if mode === "input"}
+  <input
+    type="range"
+    min="1900"
+    max="2022"
+    bind:value={current}
+    class="slider"
+    id="myRange"
+  />
+
+  <div>
+    <input
+      type="button"
+      value="확인"
+      on:click={() => onEnterHandler("result")}
+    />
+  </div>
+{:else if mode === "result"}
+  {#if current === showAnswer}
+    정답입니다!
+  {:else}
+    틀렸습니다!
+  {/if}
+
+  <div>
+    <input
+      type="button"
+      value="다시하기"
+      on:click={() => onEnterHandler("input")}
+    />
+  </div>
+{/if}
