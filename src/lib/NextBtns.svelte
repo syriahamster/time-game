00bf06f38b9e090c99e6e4da1b723607cf2d738c
Bright Kim <brightkim@pioncorp.com>
2022-11-16 23:38:06 +0900

    clean up

---
+++ src/lib/NextBtns.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let isAlive
+    export let nextQuestion
+    export let retry
+</script>
+
+{#if isAlive()}
+<div>
+  <button class="btn btn-outline" on:click={() => nextQuestion()}
+    >다음문제</button
+  >
+</div>
+{:else}
+<div>게임이 끝났습니다</div>
+<div>
+  <button class="btn btn-outline" on:click={() => retry()}
+    >다시하기</button
+  >
+</div>
+{/if}
