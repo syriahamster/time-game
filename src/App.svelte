305deac64ff2fc55704144c4841b6fd8771dcd2f
syriaHamster <the.syria.hamster@gmail.com>
2022-11-21 23:02:59 +0900

    Add Theme Selector

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,30 @@
   // import Game from "./lib/Game.svelte";
   import Home from "./lib/Home.svelte";
   import "./app.css";
+  let darkmode = localStorage.getItem('color-theme');
+  if (darkmode === null) {
+    darkmode = 'light';
+    localStorage.setItem('color-theme', darkmode);
+  }
+
+  const toggleDarkMode = () => {
+    darkmode = darkmode === 'dark' ? 'light' : 'dark';
+    if (darkmode === 'dark') document.documentElement.classList.add('dark');
+    else document.documentElement.classList.remove('dark');
+    localStorage.setItem('color-theme', darkmode);
+  };
 </script>
 
-<main>
-  <div class="card">
+<main class="bg-white dark:bg-slate-800">
+  <div class="flex flex-col h-screen p-4">
     <!-- <Game /> -->
+    <div class="flex flex-row-reverse mb-2 p-4">
+      {#if darkmode === 'light'}
+      <button class="btn bg-slate-100" on:click={toggleDarkMode}>🌙</button>
+      {:else}
+      <button class="btn bg-slate-700" on:click={toggleDarkMode}>🌞</button>
+      {/if}
+    </div>
     <Home />
   </div>
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: "class",
+  content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui')
-  ]
-}
+  plugins: [require("daisyui")],
+};
