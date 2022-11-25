59d2accb0d67b7f94820e0007b0d63b64e62b533
minhyuk <the.syria.hamster@gmail.com>
2022-11-26 00:57:44 +0900

    Add Route for Section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Home from '/src/lib/Home.svelte';
 	import '/src/app.css';
 </script>
 
@@ -9,9 +8,21 @@
 
 <main class="bg-white dark:bg-slate-800">
 	<div class="flex flex-col h-screen p-4">
-		<Home />
-		<a href="/kor_history"><button>근현대 한국사 측정기</button></a>
-		<br />
-		<p>svelte kit doc : <a href="https://kit.svelte.dev">kit.svelte.dev</a></p>
+		<h2 class="service_title">연도 맞추기</h2>
+		<br>
+		<a href="/kor_history"><button class="btn">근현대 한국사</button></a>
+		<a href="/kor_trand"><button class="btn">최신 트랜드</button></a>
+		<a href="/world_history"><button class="btn">근현대 세계사</button></a>
 	</div>
 </main>
+
+<style>
+	
+.service_title{
+    font-size: 35px;
+    font-weight: 700;
+}
+.btn {
+	margin: 10px 10px;
+}
+</style>
---
+++ src/routes/kor_trand/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import Game from '/src/lib/Game.svelte';
+    import "/src/app.css";
+    import { fade } from 'svelte/transition';
+    import History from '/src/static/quiz_kor_trend'
+
+    let is_game_mode = true
+    let quizData = History
+
+</script>
+
+<svelte:head>
+    <title>트랜드</title>
+</svelte:head>
+
+<main>
+    <div class="card">
+        <div class="quiz_section" in:fade="{{delay:200, duration:500}}">
+            <Game bind:is_game_mode
+                quizdata={quizData}>
+            </Game>
+        </div>
+    </div>
+
+</main>
+
